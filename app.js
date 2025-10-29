c2b4af19fecabe52ef95f8bee747b5a4758faf18
Nirvan004 <Nirvanandb2004@gmail.com>
2025-10-29 14:57:48 -0400

    fixed a syntax error where the addtaskbtn was not named that in the html

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let tasks = [];
 
 const taskNameInput = document.getElementById("taskName")
-const taskCategory = document.getElementById("taskCategory")
+const taskCategoryInput = document.getElementById("taskCategory")
 const taskDeadlineInput = document.getElementById('taskDeadline');
 const taskStatusInput = document.getElementById('taskStatus');
-const addTaskBtn = document.getElementById('addTaskBtn');
+const addTask = document.getElementById('addTaskBtn');
+
