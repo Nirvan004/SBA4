456b283ad8cc769c8bee2da9abe7655c2a673684
Nirvan004 <Nirvanandb2004@gmail.com>
2025-10-29 14:39:59 -0400

    made all the inputs from the html actually get read by the javascript file.

---
+++ app.js
@@ -0,0 +1,7 @@
+let tasks = [];
+
+const taskNameInput = document.getElementById("taskName")
+const taskCategory = document.getElementById("taskCategory")
+const taskDeadlineInput = document.getElementById('taskDeadline');
+const taskStatusInput = document.getElementById('taskStatus');
+const addTaskBtn = document.getElementById('addTaskBtn');
